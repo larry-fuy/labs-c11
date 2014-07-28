c4c998691b3a8876d319bc471fd9494c83a840ad
larry-fuy <larry.yong.fu@gmail.com>
2014-07-28 11:28:38 -0500

    04Ex3

--- exercises/04Ex3.cpp
+++ exercises/04Ex3.cpp
@@ -1,15 +1,39 @@
 
 #include <iostream>
+#include <exception>
+#include <stdexcept>
 
 using namespace std;
 
 // fun2
+exception_ptr fun2() {
+   exception_ptr ep;
+   try{
+      string().at(1);
+   } catch (...) {
+      ep = current_exception();
+   }
+   return ep;
+};
+
 
 // fun1
-
+void fun1() {
+//   try{
+      auto ep = fun2();     
+   if (ep) rethrow_exception(ep);
+//   } catch (const exception& e) {
+//      cout << "Caught exception in fun1" << e.what() << endl;
+//   }
+}
+      
 
 int main()
 {
-
+   try{
+      fun1();
+   } catch (const exception& e) {
+      cout << "Caught exception in main " << e.what() << endl;
+   }
 }
 
