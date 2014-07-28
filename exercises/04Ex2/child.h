56ca11683cdb341bdc963eca6abf848d24599ecd
larry-fuy <larry.yong.fu@gmail.com>
2014-07-28 10:47:42 -0500

    workable version

--- exercises/04Ex2/child.h
+++ exercises/04Ex2/child.h
@@ -5,5 +5,12 @@
 
 // Child class for section 4, exercise 2
 
+class Child : public Parent {
+public:
+   using Parent::Parent;
+   void about() override;
+   void discipline() = delete;
 
+   explicit operator int() { return age_;}
+};
 #endif
--- exercises/04Ex2/parent.h
+++ exercises/04Ex2/parent.h
@@ -1,11 +1,30 @@
 #ifndef PARENT_H
 #define PARENT_H
 
+#include <string>
+
+using namespace std;
+
 // Parent class for section 4, exercise 2
 
 class Parent
 {
 public:
+Parent(string name, int age) : name_(name), age_(age) { }
+Parent() : Parent("unnamed", 99) {}
+
+   virtual void about() {
+      cout << "Parent: < " << name_ <<">, <" << age_ << ">" << endl;
+   }
+   
+   string talk(string s) {
+      cout << " Yes, please " << endl;
+   }
+   
+   void discipline() {
+      cout << "That is not the right thing to do" << endl;
+   }
+
 
 protected:
   string name_;
